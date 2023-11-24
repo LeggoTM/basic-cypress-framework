895e075aab2f714d2f122e0170e8d3472bbefb29
LeggoTM <m.tanmay17@gmail.com>
2023-11-24 18:52:15 +0530

    first commit

---
+++ cypress/e2e/contact-us.js
@@ -0,0 +1,10 @@
+describe('Test Contact Us form', () => {
+  it('Should be able to submit form successfully', () => {
+    cy.visit('http://webdriveruniversity.com/Contact-Us/contactus.html');
+    
+  });
+
+  it('Should not be able to submit form', () => {
+    
+  });
+});
