cfbfabe9b2363103c43f3adf0c49e8c02fa96920
LeggoTM <m.tanmay17@gmail.com>
2023-11-25 09:58:59 +0530

    Add xpath plugin

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -3,7 +3,8 @@
 describe('Test Contact Us form of Automation Test Store', () => {
     it('Should be able to submit contact-us form successfully', () => {
       cy.visit('https://automationteststore.com/');
-      cy.get('a[href="https://automationteststore.com/index.php?rt=content/contact"]').click();
+      //cy.get('a[href*="/contact"]').click();
+      cy.xpath('//*[contains(@href, "/contact")]').click();
       cy.get('#ContactUsFrm_first_name').type('Tanmay');
       cy.get('#ContactUsFrm_email').type('tanmay@mail.com');
       cy.get('#ContactUsFrm_enquiry').type('Submitting this form!');
