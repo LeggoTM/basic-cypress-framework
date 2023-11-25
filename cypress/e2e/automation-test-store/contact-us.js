484e77b01618701f8901f156119681fbcde249fa
LeggoTM <m.tanmay17@gmail.com>
2023-11-25 10:37:15 +0530

    Add assertions

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -7,9 +7,10 @@ describe('Test Contact Us form of Automation Test Store', () => {
       // cy.xpath('//*[contains(@href, "/contact")]').click();
       cy.get('#ContactUsFrm_first_name').type('Tanmay');
       cy.get('#ContactUsFrm_email').type('tanmay@mail.com');
+      cy.get('#ContactUsFrm_email').should('have.value', 'tanmay@mail.com');
       cy.get('#ContactUsFrm_enquiry').type('Submitting this form!');
       cy.get('button[title="Submit"]').click();
-
+      cy.get('div.contentpanel p').eq(1).should('have.text', 'Your enquiry has been successfully sent to the store owner!');
     });
 
   });
