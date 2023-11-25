3ac2992f786ddaa26eb81aa97442014cb510f6ef
LeggoTM <m.tanmay17@gmail.com>
2023-11-25 10:05:20 +0530

    Optimize selectors

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -3,12 +3,12 @@
 describe('Test Contact Us form of Automation Test Store', () => {
     it('Should be able to submit contact-us form successfully', () => {
       cy.visit('https://automationteststore.com/');
-      //cy.get('a[href*="/contact"]').click();
-      cy.xpath('//*[contains(@href, "/contact")]').click();
+      cy.get('a[href$="/contact"]').click();
+      // cy.xpath('//*[contains(@href, "/contact")]').click();
       cy.get('#ContactUsFrm_first_name').type('Tanmay');
       cy.get('#ContactUsFrm_email').type('tanmay@mail.com');
       cy.get('#ContactUsFrm_enquiry').type('Submitting this form!');
-      cy.get('.col-md-6 > .btn').click();
+      cy.get('button[title="Submit"]').click();
 
     });
 
